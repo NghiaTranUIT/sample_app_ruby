9d6bdab6d2715a453664e06810f504b885b57f1f
NghiaTran <changeyourcolor@gmail.com>
2016-02-26 04:34:44 +0000

    Dont repeat yourself

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  #return full title of page
+  
+  def get_full_title(page_title = '')
+    
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title +" | "+ base_title;
+    end
+  end
 end
